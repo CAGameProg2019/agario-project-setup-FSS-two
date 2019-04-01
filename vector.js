f0067fbcbeec24a6eaa79e6aabe7a33914aacc0a
Loki <46567134+FSS-two@users.noreply.github.com>
2019-04-01 11:51:54 -0400

    Class

--- vector.js
+++ vector.js
@@ -1,7 +1,25 @@
 class Vector {
-    
-    constructor() {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addVector(vec){
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+//functions to write for homework
+    subVector(vec){
 
     }
 
+    scale(s){
+
+    }
+    
+    print() {
+        console.log('<'+ this.x +','+ this.y + '>');
+    }
 }
